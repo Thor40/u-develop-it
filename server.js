3e8e4de26e2118c8547aaf0415ff970d2bf75308
Thor40 <nicholas.kosik@gmail.com>
2020-06-02 18:57:50 -0500

    accessing table api with node

--- server.js
+++ server.js
@@ -27,6 +27,58 @@ app.use((req, res) => {
     res.status(404).end();
 });
 
+// captures err and rows db query response. If no errors in SQL query, err value is null
+// returns whole table
+// Get all candidates
+app.get('/api/candidates', (req, res) => {
+    const sql = `SELECT * FROM candidates`;
+    const params = [];
+    db.all(sql, params, (err, rows) => {
+      if (err) {
+        res.status(500).json({ error: err.message });
+        return;
+      }
+  
+      res.json({
+        message: 'success',
+        data: rows
+      });
+    });
+  });
+
+// start by hardcoding an id (e.g. 1) tp test expression
+// returns selected row id from table
+db.get(`SELECT * FROM candidates WHERE id = 1`, (err, row) => {
+    if(err) {
+        console.log(err);
+    }
+    console.log(row);
+});
+
+// Delete candidate
+// .run execute SQL query without retrievin data
+// ? = placeholder (PREPARED STATEMENT: has a placeholder that can be dynamically filled with real values at runtime)
+// `1` param argument provides values for prepared statement placeholders. Can be an array and hod multiple values
+// ES5 function used for callback
+db.run(`DELETE FROM candidates WHERE id = ?`, 1, function(err, result) {
+    if(err) {
+        console.log(err);
+    }
+    console.log(result, this, this.changes);
+});
+
+// Create a candidate
+const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected) 
+              VALUES (?,?,?,?)`;
+const params = [1, 'Ronald', 'Firbank', 1];
+// ES5 function, not arrow function, to use this
+db.run(sql, params, function(err, result) {
+  if (err) {
+    console.log(err);
+  }
+  console.log(result, this.lastID);
+});
+
 // start server on port 3001
 // Start server after DB connection
 db.on('open', () => {
