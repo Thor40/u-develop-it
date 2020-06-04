c36f8a4d45ce1815921ecd6a1ca8adf644792c95
Thor40 <nicholas.kosik@gmail.com>
2020-06-03 19:30:41 -0500

    all, single, delete, create

--- server.js
+++ server.js
@@ -15,56 +15,71 @@ const db = new sqlite3.Database('./db/election.db', err => {
     console.log('Connected to the election database.');
 });
 
-
 app.get('/', (req, res) => {
-    res.json({
-        message: 'Hello World'
-    });
+  res.json({
+      message: 'Hello World'
+  });
 });
 
-// Default response for any other request(Not Found) Catch all
-app.use((req, res) => {
-    res.status(404).end();
-});
 
 // captures err and rows db query response. If no errors in SQL query, err value is null
-// returns whole table
+// db.all = returns whole table
 // Get all candidates
 app.get('/api/candidates', (req, res) => {
-    const sql = `SELECT * FROM candidates`;
-    const params = [];
-    db.all(sql, params, (err, rows) => {
+  const sql = `SELECT * FROM candidates`;
+  const params = [];
+  db.all(sql, params, (err, rows) => {
+    if (err) {
+      res.status(500).json({ error: err.message });
+      return;
+    }
+
+    res.json({
+      message: 'success',
+      data: rows
+    });
+  });
+});
+
+// start by hardcoding an id (e.g. 1) tp test expression
+// db.get = returns selected row id from table
+// Get single candidate
+app.get('/api/candidate/:id', (req, res) => {
+    const sql = `SELECT * FROM candidates 
+                 WHERE id = ?`;
+    const params = [req.params.id];
+    db.get(sql, params, (err, row) => {
       if (err) {
-        res.status(500).json({ error: err.message });
+        res.status(400).json({ error: err.message });
         return;
       }
   
       res.json({
         message: 'success',
-        data: rows
+        data: row
       });
     });
   });
-
-// start by hardcoding an id (e.g. 1) tp test expression
-// returns selected row id from table
-db.get(`SELECT * FROM candidates WHERE id = 1`, (err, row) => {
-    if(err) {
-        console.log(err);
-    }
-    console.log(row);
-});
 
 // Delete candidate
 // .run execute SQL query without retrievin data
 // ? = placeholder (PREPARED STATEMENT: has a placeholder that can be dynamically filled with real values at runtime)
 // `1` param argument provides values for prepared statement placeholders. Can be an array and hod multiple values
 // ES5 function used for callback
-db.run(`DELETE FROM candidates WHERE id = ?`, 1, function(err, result) {
-    if(err) {
-        console.log(err);
+app.delete('/api/candidate/:id', (req, res) => {
+  const sql = `DELETE FROM candidates WHERE id = ?`;
+  const params = [req.params.id];
+  db.run(sql, params, function(err, result) {
+    if (err) {
+      res.status(400).json({ error: res.message });
+      return;
     }
-    console.log(result, this, this.changes);
+
+    res.json({
+      message: 'successfully deleted',
+      changes: this.changes
+    });
+  });
 });
 
 // Create a candidate
@@ -77,6 +92,11 @@ db.run(sql, params, function(err, result) {
     console.log(err);
   }
   console.log(result, this.lastID);
+});
+
+// Default response for any other request(Not Found) Catch all
+app.use((req, res) => {
+  res.status(404).end();
 });
 
 // start server on port 3001
