8ddb31286945260991c78e48d1e60cac6c7716b6
Thor40 <nicholas.kosik@gmail.com>
2020-06-03 20:54:23 -0500

    FK between tables

--- server.js
+++ server.js
@@ -27,7 +27,11 @@ app.get('/', (req, res) => {
 // db.all = returns whole table
 // Get all candidates
 app.get('/api/candidates', (req, res) => {
-  const sql = `SELECT * FROM candidates`;
+  const sql = `SELECT candidates.*, parties.name 
+                AS party_name 
+                FROM candidates 
+                LEFT JOIN parties 
+                ON candidates.party_id = parties.id`;
   const params = [];
   db.all(sql, params, (err, rows) => {
     if (err) {
@@ -46,8 +50,12 @@ app.get('/api/candidates', (req, res) => {
 // db.get = returns selected row id from table
 // Get single candidate
 app.get('/api/candidate/:id', (req, res) => {
-    const sql = `SELECT * FROM candidates 
-                 WHERE id = ?`;
+    const sql = `SELECT candidates.*, parties.name 
+                  AS party_name 
+                  FROM candidates 
+                  LEFT JOIN parties 
+                  ON candidates.party_id = parties.id 
+                  WHERE candidates.id = ?`;
     const params = [req.params.id];
     db.get(sql, params, (err, row) => {
       if (err) {
